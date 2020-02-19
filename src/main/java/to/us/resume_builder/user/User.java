82f6cc7b3acd794a0b850b118cc4cf2bbe009d33
Micah <micah.schiewe@gmail.com>
2020-02-18 21:56:26 -0600

    Add initial User framework

---
+++ src/main/java/to/us/resume_builder/user/User.java
@@ -0,0 +1,50 @@
+package to.us.resume_builder.user;
+
+/**
+ * Abstract view of a User of the system. Currently does not validate its data.
+ * 
+ * @author Micah
+ *
+ */
+public abstract class User {
+	private UserRole role;
+	private String email;
+	private transient String password;
+
+	public User(String email, String password, UserRole role) {
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (role != other.role)
+			return false;
+		return true;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+}
