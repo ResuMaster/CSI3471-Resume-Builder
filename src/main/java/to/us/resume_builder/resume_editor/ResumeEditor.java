759a32530b191c41f2dbff2bb66a038058422431
brooklynnStone1 <stone.brooklynn@gmail.com>
2020-02-19 20:56:22 -0600

    Implemented some ResumeEditor methods

--- src/main/java/to/us/resume_builder/resume_editor/ResumeEditor.java
+++ src/main/java/to/us/resume_builder/resume_editor/ResumeEditor.java
@@ -14,53 +14,113 @@ public class ResumeEditor {
     }
 
     // add experience
+    public void addExperience(ExperienceCategory c) {
+        c.addExperience();
+    }
 
-    // add field
+    // add bullet
+    public void addBullet(BulletCategory c) {
+        c.addBullet();
+    }
 
     // add header
+    public void addHeader(HeaderCategory c) {
+        resume.createCategory(CategoryType.HEADER);
+    }
 
-    // change category
-
-    // change category type
+    // change category or category type
+    public void changeCategory(Category c) {
+        // TODO implement
+    }
 
     // change experience
+    public void changeExperience(ExperienceCategory c) {
+        // TODO implement
+    }
 
-    // change field
+    // change bullet
+    public void changeBullet(BulletCategory c, Field f) {
+        // TODO implement
+    }
 
     // change header
+    public void changeHeader(HeaderCategory c) {
+        // TODO implement
+    }
 
 
     // remove category
+    public void removeCategory(Category c) {
+        // TODO implement
+    }
 
     // remove experience
+    public void removeExperience(ExperienceCategory c, Experience e) {
+        c.removeExperience(e.getId());
+    }
 
-    // remove field
+    // remove bullet
+    public void removeBullet(BulletCategory c, Field f) {
+        c.removeBullet(f.getId());
+    }
 
     // remove header
+    public void removeHeader(HeaderCategory c) {
 
+    }
 
     // set toggle field
+    public void toggleField(Field f) {
+        f.setVisible(!f.getVisible());
+    }
 
     // set toggle experience
+    public void toggleExperience(Experience e) {
+        e.setVisible(!e.getVisible());
+    }
 
     // set toggle category
+    public void toggleCategory(Category c) {
+        c.setVisible(!c.getVisible());
+    }
 
     // set toggle header
+    public void toggleHeader(HeaderCategory c) {
+        c.setVisible(!c.getVisible());
+    }
 
     // preview resume
+    public void previewResume() {
+        // TODO implement
+    }
 
 
     // export resume
+    public void exportResume() {
+        // TODO implement
+    }
 
     // cancel export resume
+    public void cancelExport() {
+        // TODO implement
+    }
 
     // set save location
+    public void saveLocation() {
+        // TODO implement
+    }
 
 
     // email resume
+    public void emailResume() {
+        // TODO implement
+    }
 
 
     // import data file
+    public void importDataFile() {
+        // TODO implement
+    }
 
     // export data file
 
