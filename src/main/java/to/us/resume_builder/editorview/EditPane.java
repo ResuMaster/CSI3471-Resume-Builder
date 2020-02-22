ab1088df3670bcc3751e887b15e3d54ed1df0397
Micah <micah_schiewe1@baylor.edu>
2020-02-21 18:02:11 -0600

    Fix formatting
    * Formatter reverted to using tabs; this fixes it.

--- src/main/java/to/us/resume_builder/editorview/EditPane.java
+++ src/main/java/to/us/resume_builder/editorview/EditPane.java
@@ -10,14 +10,14 @@ import javax.swing.JPanel;
  */
 public abstract class EditPane extends JPanel {
 
-	/**
-	 * Version on 2/21.
-	 */
-	private static final long serialVersionUID = 3001154674271049780L;
+    /**
+     * Version on 2/21.
+     */
+    private static final long serialVersionUID = 3001154674271049780L;
 
-	/**
-	 * Saves the data in this EditPane to the copy of the ResumeData <em>in
-	 * ram</em>.
-	 */
-	abstract void save();
+    /**
+     * Saves the data in this EditPane to the copy of the ResumeData <em>in
+     * ram</em>.
+     */
+    abstract void save();
 }
