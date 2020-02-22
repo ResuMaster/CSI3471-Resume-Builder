fdb062edcb851e5446437e9b29e2b9e096f9b8ed
Micah <micah_schiewe1@baylor.edu>
2020-02-21 20:20:04 -0600

    Implement rough EditorStage component
    * EditorStage: The central part of the Editor component, which enables
      the actual editing.

--- src/main/java/to/us/resume_builder/editorview/EditorStage.java
+++ src/main/java/to/us/resume_builder/editorview/EditorStage.java
@@ -1,18 +1,54 @@
 package to.us.resume_builder.editorview;
 
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 
 import to.us.resume_builder.resume_components.category.Category;
+import to.us.resume_builder.resume_components.category.TextCategory;
 
 public class EditorStage extends JPanel {
+    private JPanel editContainer;
+    private EditorCategoryHeader header;
     private EditPane edit;
 
-    public EditorStage() {
-        //
+    public EditorStage(Category startingCategory) {
+        JButton saveButton;
+        JScrollPane scroller;
+
+        // Create central UI
+        editContainer = new JPanel();
+        editContainer.setLayout(new BoxLayout(editContainer, BoxLayout.Y_AXIS));
+
+        header = new EditorCategoryHeader(startingCategory);
+        edit = getEditor(startingCategory);
+        editContainer.add(header);
+        editContainer.add(edit);
+        scroller = new JScrollPane(editContainer, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        // Save button
+        saveButton = new JButton("Save");
+        saveButton.addActionListener(e -> {
+            edit.save();
+        });
+        saveButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Assemble.
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        add(Box.createVerticalGlue());
+        add(scroller);
+        add(Box.createRigidArea(new Dimension(0, 5)));
+        add(saveButton);
     }
 
     /**
@@ -23,25 +59,36 @@ public class EditorStage extends JPanel {
     public void showInEditor(Category toEdit) {
         if (edit != null)
             remove(edit);
+        edit = getEditor(toEdit);
+        add(edit);
+        revalidate();
+    }
 
+    /**
+     * Generates the proper editor component for the provided category.
+     * 
+     * @param toEdit The category to get an editor for
+     * @return A GUI editor for the provided {@link Category}
+     */
+    private EditPane getEditor(Category toEdit) {
+        EditPane editPane = null;
         // This switch controls creating different editors for each CategoryType
         // TODO Connect with A&B's editors
         switch (toEdit.getType()) {
-        case BULLETS:
-            break;
-        case EXPERIENCE:
-            break;
-        case HEADER:
-            break;
-        case TEXT:
-            break;
+//        case BULLETS:
+//            break;
+//        case EXPERIENCE:
+//            break;
+//        case HEADER:
+//            break;
+//        case TEXT:
+//            break;
         default:
-            edit = new ConcreteEditPane(toEdit.getDisplayName());
+            editPane = new ConcreteEditPane(toEdit.getDisplayName());
             break;
         }
 
-        add(edit);
-        revalidate();
+        return editPane;
     }
 
     /**
@@ -69,7 +116,7 @@ public class EditorStage extends JPanel {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame f = new JFrame("Tester");
-            f.add(new EditorStage());
+            f.add(new EditorStage(new TextCategory("heya")));
             f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             f.pack();
             f.setVisible(true);
