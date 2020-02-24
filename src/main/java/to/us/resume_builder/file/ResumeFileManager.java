e88d54de4d22407c85f74758d3154e28d39483a3
nnazo <jacobcurtis2266@gmail.com>
2020-02-23 22:34:07 -0600

    fix ResumeFileManager after pulling origin

--- src/main/java/to/us/resume_builder/file/ResumeFileManager.java
+++ src/main/java/to/us/resume_builder/file/ResumeFileManager.java
@@ -11,28 +11,13 @@ import java.text.DateFormat;
  * @author Jacob
  */
 public class ResumeFileManager {
-<<<<<<< HEAD
     // Used for marshalling/unmarshalling json
     private static Gson gson = new GsonBuilder()
-=======
-    Gson gson;
-
-    /**
-     * Constructs a ResumeFileManager
-     */
-    public ResumeFileManager() {
-        super();
-        gson = new GsonBuilder()
->>>>>>> 6a47fc88421d6d301c1dd11bf2a9d25b34517c2d
-            .serializeNulls()
-            .setDateFormat(DateFormat.FULL)
-            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-            .setPrettyPrinting()
-            .create();
-<<<<<<< HEAD
-=======
-    }
->>>>>>> 6a47fc88421d6d301c1dd11bf2a9d25b34517c2d
+        .serializeNulls()
+        .setDateFormat(DateFormat.FULL)
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .setPrettyPrinting()
+        .create();
 
     /**
      * Imports a given resume data JSON file.
@@ -41,11 +26,7 @@ public class ResumeFileManager {
      * @return The ResumeFile read from the file.
      * @throws IOException
      */
-<<<<<<< HEAD
     public static ResumeFile importFile(String path) throws IOException {
-=======
-    public ResumeFile importFile(String path) throws IOException {
->>>>>>> 6a47fc88421d6d301c1dd11bf2a9d25b34517c2d
         Reader file = new FileReader(path);
         ResumeFile r = null;
         try {
@@ -67,11 +48,7 @@ public class ResumeFileManager {
      * @param path The path to the file to export to.
      * @throws IOException
      */
-<<<<<<< HEAD
     public static void exportFile(ResumeFile r, String path) throws IOException {
-=======
-    public void exportFile(ResumeFile r, String path) throws IOException {
->>>>>>> 6a47fc88421d6d301c1dd11bf2a9d25b34517c2d
         String json = gson.toJson(r);
         Writer file = new FileWriter(path);
         file.write(json);
