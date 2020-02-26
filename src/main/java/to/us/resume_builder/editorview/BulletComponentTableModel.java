28540ec5f31a812017a04f6d5ed3d77a225701f1
AshleyLuEllen <ashleylu_couch1@baylor.edu>
2020-02-26 10:42:39 -0600

    Added JavaDoc comments

--- src/main/java/to/us/resume_builder/editorview/BulletComponentTableModel.java
+++ src/main/java/to/us/resume_builder/editorview/BulletComponentTableModel.java
@@ -15,36 +15,91 @@ public class BulletComponentTableModel extends AbstractTableModel implements Tab
         this.data = data;
     }
 
+
+    /**
+     * Returns the number of rows in the model. A
+     * <code>JTable</code> uses this method to determine how many rows it
+     * should display.  This method should be quick, as it
+     * is called frequently during rendering.
+     *
+     * @return the number of rows in the model
+     * @see #getColumnCount
+     */
     @Override
     public int getRowCount() {
         return data.size();
     }
 
+    /**
+     * Returns the number of columns in the model. A
+     * <code>JTable</code> uses this method to determine how many columns it
+     * should create and display by default.
+     *
+     * @return the number of columns in the model
+     * @see #getRowCount
+     */
     @Override
     public int getColumnCount() {
         return columnNames.length;
     }
 
+    /**
+     * Returns a default name for the column using spreadsheet conventions:
+     * A, B, C, ... Z, AA, AB, etc.  If <code>column</code> cannot be found,
+     * returns an empty string.
+     *
+     * @param column the column being queried
+     * @return a string containing the default name of <code>column</code>
+     */
     @Override
     public String getColumnName(int column) {
         return columnNames[column];
     }
 
+    /**
+     * Returns <code>Object.class</code> regardless of <code>columnIndex</code>.
+     *
+     * @param columnIndex the column being queried
+     * @return the Object.class
+     */
     @Override
     public Class<?> getColumnClass(int columnIndex) {
         return data.get(columnIndex).getClass();
     }
 
+    /**
+     * Returns false.  This is the default implementation for all cells.
+     *
+     * @param rowIndex    the row being queried
+     * @param columnIndex the column being queried
+     * @return false
+     */
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
         return true;
     }
 
+    /**
+     * Returns the value for the cell at <code>columnIndex</code> and
+     * <code>rowIndex</code>.
+     *
+     * @param rowIndex    the row whose value is to be queried
+     * @param columnIndex the column whose value is to be queried
+     * @return the value Object at the specified cell
+     */
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return null;
+        return data.get(columnIndex)[rowIndex];
     }
 
+    /**
+     * This empty implementation is provided so users don't have to implement
+     * this method if their data model is not editable.
+     *
+     * @param aValue      value to assign to cell
+     * @param rowIndex    row of cell
+     * @param columnIndex column of cell
+     */
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
         super.setValueAt(aValue, rowIndex, columnIndex);
