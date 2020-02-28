7b3cdf9fecbd5114f7795937db781c5aba682f9e
nnazo <jacobcurtis2266@gmail.com>
2020-02-27 23:44:02 -0600

    add EditorCategorySelected, to be implemented

---
+++ src/main/java/to/us/resume_builder/editorview/EditorCategorySelector.java
@@ -0,0 +1,56 @@
+package to.us.resume_builder.editorview;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EditorCategorySelector extends JPanel implements ListSelectionListener {
+    private JList<String> categories;
+    private JScrollPane scroll;
+
+    public EditorCategorySelector() {
+        categories = new JList<>();
+        scroll = new JScrollPane();
+
+        categories.addListSelectionListener(this);
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() > 1) {
+                    // TODO implement
+                    int index = categories.locationToIndex(e.getPoint());
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // TODO implement
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                // TODO implement
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                // TODO implement
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // TODO implement
+            }
+        });
+
+        scroll.add(categories);
+        scroll.getViewport().setView(categories);
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+
+    }
+}
