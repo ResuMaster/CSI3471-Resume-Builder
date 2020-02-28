14b4eb9462d9b869266a564f455e2ceb97349521
nnazo <jacobcurtis2266@gmail.com>
2020-02-27 23:46:46 -0600

    move UI test to Main.java

--- src/main/java/to/us/resume_builder/Main.java
+++ src/main/java/to/us/resume_builder/Main.java
@@ -1,7 +1,24 @@
 package to.us.resume_builder;
 
+import to.us.resume_builder.editorview.EditorCategorySelector;
+import to.us.resume_builder.editorview.EditorMenuBar;
+import to.us.resume_builder.editorview.EditorStage;
+import to.us.resume_builder.resume_components.category.TextCategory;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        SwingUtilities.invokeLater(() -> {
+            JFrame f = new JFrame("Tester");
+            f.setLayout(new BorderLayout());
+            f.setJMenuBar(new EditorMenuBar());
+            f.add(new EditorCategorySelector(), BorderLayout.WEST);
+            f.add(new EditorStage(new TextCategory("heya")), BorderLayout.CENTER);
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.pack();
+            f.setVisible(true);
+        });
     }
 }
