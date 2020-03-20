596b21cd498a0a2eda1dbc595e3639b4c5825351
nnazo <jacobcurtis2266@gmail.com>
2020-03-19 23:19:29 -0500

    Require file selection before editor is opened, fix javadoc

--- src/main/java/to/us/resume_builder/Main.java
+++ src/main/java/to/us/resume_builder/Main.java
@@ -3,15 +3,29 @@ package to.us.resume_builder;
 import to.us.resume_builder.main_window.*;
 import to.us.resume_builder.file.ResumeFile;
 import to.us.resume_builder.file.ResumeFileManager;
+import to.us.resume_builder.util.FileDialog;
 
 import javax.swing.*;
 import java.io.IOException;
 
+/**
+ * The Main class to be run.
+ *
+ * @author Jacob Curtis
+ * @author Micah Schiewe
+ */
 public class Main {
+    /**
+     * Retrieve the file to be opened and then load the editor.
+     *
+     * @param args Commands line arguments, unused.
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            FileRetriever t = new FileRetriever();
+            String file = t.getFile();
             try {
-                ResumeFile rf = ResumeFileManager.importFile("./src/main/resources/test.json");
+                ResumeFile rf = ResumeFileManager.importFile(file);
                 if (rf != null) {
                     new EditorFrame(rf);
                 }
@@ -19,5 +33,34 @@ public class Main {
                 e.printStackTrace();
             }
         });
+    }
+}
+
+/**
+ * Class that will prompt the user to select a Resume Data File before
+ * the application will open.
+ *
+ * @author Jacob Curtis
+ */
+class FileRetriever extends JPanel {
+    /**
+     * File dialog to retrieve the file from.
+     */
+    private FileDialog fileDialog;
+
+    /**
+     * Constructs the file retriever to only accept JSON.
+     */
+    public FileRetriever() {
+        fileDialog = new FileDialog("json", this);
+    }
+
+    /**
+     * Gets the Resume Data File the user wants to open in the edtior.
+     *
+     * @return The selected file.
+     */
+    public String getFile() {
+        return fileDialog.getFile();
     }
 }
