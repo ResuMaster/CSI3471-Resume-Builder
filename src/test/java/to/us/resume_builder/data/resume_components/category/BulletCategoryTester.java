cb93a77421bc842b625cda3bfe4da23ff592b476
brooklynnStone1 <stone.brooklynn@gmail.com>
2020-04-19 18:41:30 -0500

    Testing for TextCategory done

--- src/test/java/to/us/resume_builder/data/resume_components/category/BulletCategoryTester.java
+++ src/test/java/to/us/resume_builder/data/resume_components/category/BulletCategoryTester.java
@@ -1,6 +1,5 @@
 package to.us.resume_builder.data.resume_components.category;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
@@ -12,11 +11,18 @@ import to.us.resume_builder.data.resume_components.Resume;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Test class for the {@link BulletCategory}
+ * @author Brooklynn Stone
+ */
 public class BulletCategoryTester {
     private static Resume r;
     private static BulletCategory bc;
     private static List<Bullet> bullets;
 
+    /**
+     * Initializes the resume and 10 bullets with text "Bullet #."
+     */
     @BeforeEach
     public void init() {
         r = new Resume();
@@ -29,6 +35,9 @@ public class BulletCategoryTester {
         }
     }
 
+    /**
+     * Verify that the getter returns all 10 bullets with the expected text and identifications
+     */
     @Test
     public void testGetBulletList() {
         for(int i = 0; i < bullets.size(); i++) {
@@ -37,17 +46,27 @@ public class BulletCategoryTester {
         }
     }
 
+    /**
+     * Verify that setting the column changes the column internally
+     */
     @Test
     public void testSetColumn() {
         bc.setColumn(2);
         assertEquals(2, bc.getColumn());
     }
 
+    /**
+     * Verify that getting the column number returns 1 for an initialized {@link Bullet}
+     */
     @Test
     public void testGetColumn() {
         assertEquals(1, bc.getColumn());
     }
 
+    /**
+     * Check that the {@link Bullet} returned from this method returns a {@link Bullet} with the same
+     * text and identification
+     */
     @Test
     public void testGetBulletByID() {
         assertEquals(bullets.get(2).getText()+bullets.get(2).getID(),
@@ -55,6 +74,10 @@ public class BulletCategoryTester {
                         bc.getBulletByID(bullets.get(2).getID()).getID());
     }
 
+    /**
+     * Add a new {@link Bullet} to the {@link BulletCategory} and verify that the {@link BulletCategory}
+     * contains the new {@link Bullet} and that the size of the bullet list is one more.
+     */
     @Test
     public void testAddBullet() {
         String id = bc.addBullet();
@@ -62,6 +85,10 @@ public class BulletCategoryTester {
         assertEquals(11, bc.getBulletList().size());
     }
 
+    /**
+     * Add a {@link Bullet} which has already been initialized to the {@link BulletCategory}
+     * and verify that the size is correct and that the {@link Bullet} was added to the end.
+     */
     @Test
     public void testAddBullet2() {
         Bullet b = new Bullet("12345");
@@ -74,6 +101,10 @@ public class BulletCategoryTester {
                 equals(b.getID()+b.getText()+b.getVisible()));
     }
 
+    /**
+     * Remove a {@link Bullet} from the {@link BulletCategory} and verify that it cannot be
+     * retrieved from the Bullet list which is one smaller than it was before.
+     */
     @Test
     public void testRemoveBullet() {
         bc.removeBullet(bullets.get(0).getID());
@@ -81,18 +112,27 @@ public class BulletCategoryTester {
         assertEquals(9, bc.getBulletList().size());
     }
 
+    /**
+     * Verify that an expected {@link Bullet} returns true
+     */
     @Test
     public void testCheckBulletListID() {
         Bullet temp = bullets.get(0);
         assertTrue(bc.checkBulletListID(temp.getID()));
     }
 
+    /**
+     * Verify that an unexpected {@link Bullet} returns false
+     */
     @Test
     public void testCheckBulletListID2() {
         Bullet temp = new Bullet("1234");
         assertFalse(bc.checkBulletListID(temp.getID()));
     }
 
+    /**
+     * Verify the correct formatting of a LaTeX String with all 10 {@link Bullet}s
+     */
     @Test
     public void testFormatLaTeXString() {
         String s = "\\begin{resumebulletcategory}{}\n    ";
---
+++ src/test/java/to/us/resume_builder/data/resume_components/category/TextCategoryTester.java
@@ -0,0 +1,70 @@
+package to.us.resume_builder.data.resume_components.category;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import to.us.resume_builder.business.export_LaTeX.ResumeTemplate;
+import to.us.resume_builder.data.resume_components.Resume;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for the {@link TextCategory}
+ * @author Brooklynn Stone
+ */
+public class TextCategoryTester {
+    /**
+     * Test resume which generates an id for the {@link BulletCategory}
+     */
+    private static Resume r;
+
+    /**
+     * Test {@link TextCategory} to verify set, get, and LaTeX generation
+     */
+    private static TextCategory tc;
+
+    /**
+     * Initializes the {@link Resume} and the {@link TextCategory} with "The Text"
+     */
+    @BeforeEach
+    public void init() {
+        r = new Resume();
+        tc = new TextCategory(r.createCategory(CategoryType.BULLETS));
+        tc.setText("The text");
+    }
+
+    /**
+     * Sets the text to something else and verifies that this change has been made internally
+     */
+    @Test
+    public void testSetText() {
+        tc.setText("This is the text");
+        assertEquals("This is the text", tc.getText());
+    }
+
+    /**
+     * Verifies the text is being stored correctly internally
+     */
+    @Test
+    public void testGetText() {
+        assertEquals("The text", tc.getText());
+    }
+
+    /**
+     * Sets the text to something else and verifies that this text is returned as expected
+     */
+    @Test
+    public void testGetText2() {
+        tc.setText("More text");
+        assertEquals("More text", tc.getText());
+    }
+
+    /**
+     * Verify that the LaTeX string is generated in the expected manner
+     */
+    @Test
+    public void testFormatLaTeXString() {
+        tc.setText("This is the text");
+
+        assertEquals("\\resumetextcategory{}{This is the text}\n\n", tc.formatLaTeXString(ResumeTemplate.DEFAULT));
+    }
+}
