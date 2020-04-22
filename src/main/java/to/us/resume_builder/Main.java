d5e97d4323bc8a33f7d6f189f3cadbea6762be33
Micah <micah_schiewe1@baylor.edu>
2020-04-22 18:59:43 -0500

    Set up logging to file for project

--- src/main/java/to/us/resume_builder/Main.java
+++ src/main/java/to/us/resume_builder/Main.java
@@ -1,12 +1,20 @@
 package to.us.resume_builder;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+import java.util.logging.LogManager;
+
+import javax.swing.SwingUtilities;
+
 import to.us.resume_builder.business.resume_file_management.ResumeFile;
 import to.us.resume_builder.business.resume_file_management.ResumeFileManager;
-import to.us.resume_builder.presentation.EditorFrame;
 import to.us.resume_builder.business.util.FileDialog;
-
-import javax.swing.*;
-import java.io.IOException;
+import to.us.resume_builder.presentation.EditorFrame;
 
 /**
  * The Main class to be run.
@@ -15,6 +23,34 @@ import java.io.IOException;
  * @author Micah Schiewe
  */
 public class Main {
+    private static final String LOG_CONFIG_FILE = "log.properties";
+
+    /**
+     * Initialize the logger.
+     */
+    static {
+        try (InputStream reader = Main.class.getClassLoader().getResourceAsStream(LOG_CONFIG_FILE)) {
+            // Read in the configurations
+            LogManager lm = LogManager.getLogManager();
+            lm.readConfiguration(reader);
+        } catch (IOException | InvalidPathException ex) {
+            // Error: could not read configurations, report the failure
+            ex.printStackTrace();
+            try (PrintWriter writer = new PrintWriter(
+                    Files.newBufferedWriter(Paths.get("report_log_failure"), StandardCharsets.UTF_8))) {
+                writer.println("Could not find " + LOG_CONFIG_FILE + "; logging via stdout as a fallback. Reason:");
+                ex.printStackTrace(writer);
+                writer.flush();
+            } catch (IOException | InvalidPathException e2) {
+                // Error reporting the error
+                e2.printStackTrace();
+                System.err.println("Caused by attempting to report:");
+                ex.printStackTrace();
+                System.err.println("Failed to persist error record");
+            }
+        }
+    }
+
     /**
      * Retrieve the file to be opened and then load the editor.
      *
