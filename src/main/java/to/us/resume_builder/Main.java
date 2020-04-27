f4c1c9877aa7947bde54e081f6d54f630a234ff1
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-04-26 22:25:51 -0700

    Logging now in a file

--- src/main/java/to/us/resume_builder/Main.java
+++ src/main/java/to/us/resume_builder/Main.java
@@ -3,14 +3,15 @@ package to.us.resume_builder;
 import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import to.us.resume_builder.business.ApplicationConfiguration;
-import to.us.resume_builder.business.resume_file_management.ResumeFile;
-import to.us.resume_builder.business.resume_file_management.ResumeFileManager;
 import to.us.resume_builder.presentation.CreateOrOpenDialog;
-import to.us.resume_builder.presentation.EditorFrame;
-import to.us.resume_builder.business.util.FileDialog;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * The Main class to be run.
@@ -19,12 +20,22 @@ import java.io.IOException;
  * @author Micah Schiewe
  */
 public class Main {
+    private static Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     /**
      * Retrieve the file to be opened and then load the editor.
      *
      * @param args Commands line arguments, unused.
      */
     public static void main(String[] args) {
+        // Setup logger
+        LogManager lm = LogManager.getLogManager();
+        try {
+            lm.readConfiguration(Thread.currentThread().getContextClassLoader().getResourceAsStream("log.properties"));
+        } catch (IOException e) {
+            LOGGER.severe("Couldn't read configuration file.");
+        }
+
         SwingUtilities.invokeLater(() -> {
             try {
                 switch (ApplicationConfiguration.getInstance().getString("theme.color")) {
