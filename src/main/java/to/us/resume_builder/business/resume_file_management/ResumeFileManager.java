0feb6deaa4bed256aea01a693b7c9169f0aa0b39
nnazo <jacobcurtis2266@gmail.com>
2020-04-28 18:33:59 -0500

    extract import/export functionality to methods that accept reader/writer arguments

--- src/main/java/to/us/resume_builder/business/resume_file_management/ResumeFileManager.java
+++ src/main/java/to/us/resume_builder/business/resume_file_management/ResumeFileManager.java
@@ -4,6 +4,7 @@ import com.google.gson.*;
 import to.us.resume_builder.data.resume_components.category.Category;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.util.Collection;
 import java.util.logging.Logger;
@@ -37,19 +38,32 @@ public class ResumeFileManager {
      */
     public static ResumeFile importFile(String path) throws IOException {
         LOG.info("Importing resume data file from " + path);
-        Reader file = new FileReader(path);
+        ResumeFile file = importFile(new BufferedReader(new FileReader(path, StandardCharsets.UTF_8)));
+        if (file != null) {
+            file.setFilePath(path);
+        }
+        return file;
+    }
+
+    /**
+     * Imports a given resume data JSON reader.
+     *
+     * @param reader The reader containing the JSON data to be imported.
+     * @return The ResumeFile read from the file.
+     * @throws IOException exception opening, closing or reading from json file
+     */
+    public static ResumeFile importFile(Reader reader) throws IOException {
         ResumeFile r = null;
         try {
             LOG.info("Attempting to deserialize ResumeFile");
-            r = gson.fromJson(file, ResumeFile.class);
+            r = gson.fromJson(reader, ResumeFile.class);
         } catch (JsonSyntaxException | JsonIOException e) {
             LOG.warning("ResumeFile deserializaiton failed: " + e);
             return null;
         } finally {
-            file.close();
+            reader.close();
         }
         LOG.info("ResumeFile was successfully deserialized.");
-        r.setFilePath(path);
         return r;
     }
 
@@ -62,11 +76,20 @@ public class ResumeFileManager {
      */
     public static void exportFile(ResumeFile r, String path) throws IOException {
         LOG.info("Serializing ResumeFile for export");
+        exportFile(r, new BufferedWriter(new FileWriter(path, StandardCharsets.UTF_8)));
+    }
+
+    /**
+     * Exports a given ResumeFile to the requested Writer.
+     *
+     * @param r The ResumeFile to be exported
+     * @param writer The path to the file to export to.
+     * @throws IOException exception creating, closing or writing to json file
+     */
+    public static void exportFile(ResumeFile r, Writer writer) throws IOException {
         String json = gson.toJson(r);
-        Writer file = new FileWriter(path);
-        LOG.info("Attempting to write ResumeFile JSON to " + path);
-        file.write(json);
-        file.close();
+        writer.write(json);
+        writer.close();
     }
 
 }
