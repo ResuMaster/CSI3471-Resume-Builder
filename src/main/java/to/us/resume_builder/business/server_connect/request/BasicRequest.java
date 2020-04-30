c8ae670153a11aaa6af3087c813b7aef5bec63cb
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-04-29 19:29:26 -0700

    Up timeout

--- src/main/java/to/us/resume_builder/business/server_connect/request/BasicRequest.java
+++ src/main/java/to/us/resume_builder/business/server_connect/request/BasicRequest.java
@@ -45,7 +45,7 @@ public abstract class BasicRequest<T> {
 	protected static final String PARAM_SPLIT = "?";
 
 	/** Assumed timeout, in seconds, of the method */
-	protected static final int TIMEOUT = 15;
+	protected static final int TIMEOUT = 60;
 
 	/** The type of request this handles */
 	private RequestType type;
